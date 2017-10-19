bcc9a1d5c2b18c787b500c38e81ffbff7cc11128
ascrutae <nzt48.xin@gmail.com>
2017-10-19 23:40:02 +0800

    suit new version of skywalking

--- docker/catalina.sh
+++ docker/catalina.sh
@@ -106,7 +106,7 @@
 
 export AGENT_FILE_PATH=/usr/local/tomcat/agent
 if [ -f "${AGENT_FILE_PATH}/skywalking-agent.jar" ]; then
-    CATALINA_OPTS="$CATALINA_OPTS -javaagent:${AGENT_FILE_PATH}/skywalking-agent.jar -DconfigPath=/usr/local/tomcat/agent-config"
+    CATALINA_OPTS="$CATALINA_OPTS -javaagent:${AGENT_FILE_PATH}/skywalking-agent.jar -Dskywalking.collector.grpc_channel_check_interval=2 -Dskywalking.collector.app_and_service_register_check_interval=2 -Dcollector.discovery_check_interval=2 -Dskywalking.collector.servers=${COLLECTOR_SERVER} -Dskywalking.agent.application_code=jedis-standalone-case"
 fi
 
 # OS specific support.  $var _must_ be set to either true or false.
--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-echo "replace {COLLECTOR_SERVER} to $COLLECTOR_SERVER"
-eval sed -i -e 's/\{COLLECTOR_SERVER\}/$COLLECTOR_SERVER/' /usr/local/tomcat/agent-config/sky-walking.config
-
 echo "replace {REDIS_HOST} to $REDIS_HOST"
 eval sed -i -e 's/\{REDIS_HOST\}/$REDIS_HOST/' /usr/local/tomcat/webapps/jedis-standalone-case/WEB-INF/classes/jedis.properties
 
